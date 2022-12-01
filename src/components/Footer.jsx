c5711466fb7e24b11fdf3822bb0e73ea8987f10c
daniel-alarcon97 <danny.alarcon1997@gmail.com>
2022-11-30 22:10:31 -0500

    first challenge

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+var date = new Date().getFullYear();
+
+function Footer() {
+  return <footer>Copyright {date}</footer>;
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>In the Jungle the Mighty Jungle</p>
+    </div>
+  );
+}
+
+export default Note;
